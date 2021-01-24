9112c8586786c2730f72ab41e4a4fd6d5929788e
Ahmaud R. Templeton <ahmaud@gmail.com>
2021-01-24 00:08:49 -0500

    commit tree baf24f8cd99dc5a062a64dd0470592069b555b59

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    lowest = nil
+    name_hash.each do |item, price|
+        if price == 1 || price == 10
+            lowest = item
+        elsif
+            name_hash.empty? == true
+            return nil
+        end
+    end
+    lowest
 end
